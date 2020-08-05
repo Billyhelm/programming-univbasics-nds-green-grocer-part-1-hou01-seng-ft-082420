7a7cc7c40e0ba9c2b88ea65cf236d67e680c2b59
Billy Helm <billyhelm88@gmail.com>
2020-08-05 15:53:44 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,7 +5,7 @@ end
 
 
 def consolidate_cart(cart)
-  new_hash = cart.reduce() {|key| find_item_by_name_in_collection(key[:item],new_hash) ? cart[key][:count] += 1 : cart[key][:count] = 1}
+  new_hash = cart.reduce{|key| find_item_by_name_in_collection(key[:item],new_hash) ? cart[key][:count] += 1 : cart[key][:count] = 1}
   new_hash
 end
 
