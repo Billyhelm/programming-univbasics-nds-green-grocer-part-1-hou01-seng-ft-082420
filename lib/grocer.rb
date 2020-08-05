87e9389db021b88e6dcbb50d7fb8f1b4abbf29bd
Billy Helm <billyhelm88@gmail.com>
2020-08-05 16:11:51 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def find_item_by_name_in_collection(item, cart)
   new_hash = cart.select {|hash| hash[:item] == item}
   new_hash[0]
@@ -7,6 +9,7 @@ end
 def consolidate_cart(cart)
   new_array = cart.each do |hash|
     find_item_by_name_in_collection(hash[:item],new_array) ? hash[:count] += 1 : hash[:count] = 1
+    binding.pry
   end 
   new_array
 end
