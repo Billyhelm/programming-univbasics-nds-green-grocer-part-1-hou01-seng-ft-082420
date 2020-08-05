2a89cd10584ce25833ce9ad2dcfa4de41dfb83fc
Billy Helm <billyhelm88@gmail.com>
2020-08-05 15:32:13 +0000

    Automatically backed up by Learn

--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -26,15 +26,7 @@ end
 def find_item_by_name_in_collection(item, cart)
   new_hash = cart.select {|hash| hash[:item] == item}
   new_hash[0]
-  
 end
-test_cart = [
-        { :item => "DOG FOOD" },
-        { :item => "WINE" },
-        { :item => "STRYCHNINE" }
-      ]
-
-find_item_by_name_in_collection("WINE", test_cart)
 
 def generate_cart
 	[].tap do |cart|
